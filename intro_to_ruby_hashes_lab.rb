14807dcdc74437b01208ecb2546969febafe6c2b
Connor Blakeney <connorblakeney@yahoo.com>
2020-02-18 16:45:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,8 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  read_from_hash = {}
+  read_from_hash = {hash => key}
+  [:hash]
 end
 
 def update_counting_hash(hash, key)
